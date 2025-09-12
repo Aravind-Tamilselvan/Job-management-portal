9124986cd06269f91c603512ca89d3b25b5b75a4
AswanthTamilselvan <aswantht11@gmail.com>
2025-09-12 18:18:07 +0530

    Final commit

--- server/index.js
+++ server/index.js
@@ -13,8 +13,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+
+const __dirname = path.resolve();
 
 // Middlewares
 app.use(cors({
